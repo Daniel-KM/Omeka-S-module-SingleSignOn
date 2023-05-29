4a23e697b6668e60b6fb638d682829b74794beda
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-29 00:00:00 +0000

    Released version 3.4.5.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -24,8 +24,35 @@ $messenger = $plugins->get('messenger');
 $entityManager = $services->get('Omeka\EntityManager');
 
 if (version_compare($oldVersion, '3.4.3', '<')) {
-    $settings->set('singlesignon_sp_metadata_mode', $settings->get('singlesignon_metadata_mode') ?: 'basic');
+    $settings->set('singlesignon_sp_metadata_mode', $settings->get('singlesignon_metadata_mode') ?: 'standard');
     $settings->set('singlesignon_idp_attributes_map', $settings->get('singlesignon_attributes_map') ?: []);
     $settings->delete('singlesignon_metadata_mode');
     $settings->delete('singlesignon_attributes_map');
+}
+
+if (version_compare($oldVersion, '3.4.5', '<')) {
+    $idp = [
+        'idp_entity_id' => $settings->get('singlesignon_idp_entity_id', ''),
+        'idp_sso_url' => $settings->get('singlesignon_idp_sso_url', ''),
+        'idp_slo_url' => $settings->get('singlesignon_idp_slo_url', ''),
+        'idp_x509_certificate' => $settings->get('singlesignon_idp_x509_certificate', ''),
+        'idp_attributes_map' => $settings->get('singlesignon_idp_attributes_map', []),
+    ];
+    $entityId = $idp['idp_entity_id'] ?? '';
+    if (substr($entityId, 0, 4) !== 'http') {
+        $entityId = 'http://' . $entityId;
+    }
+    $idpName = parse_url($entityId, PHP_URL_HOST) ?: 0;
+    $settings->set('singlesignon_idps', [$idpName => $idp]);
+
+    $settings->delete('singlesignon_idp_entity_id');
+    $settings->delete('singlesignon_idp_sso_url');
+    $settings->delete('singlesignon_idp_slo_url');
+    $settings->delete('singlesignon_idp_x509_certificate');
+    $settings->delete('singlesignon_idp_attributes_map');
+
+    $message = new Message(
+        'It is possible now to manage multipe IdPs.' // @translate
+    );
+    $messenger->addSuccess($message);
 }

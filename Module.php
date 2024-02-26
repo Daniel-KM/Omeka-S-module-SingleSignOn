593108043d8b87ac57db7395d74767abb0760da4
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-02-26 00:00:00 +0000

    Released version 3.4.9 for Omeka S v4.

--- Module.php
+++ Module.php
@@ -48,6 +48,21 @@ class Module extends AbstractModule
             );
     }
 
+    protected function preInstall(): void
+    {
+        $services = $this->getServiceLocator();
+        $plugins = $services->get('ControllerPluginManager');
+        $translate = $plugins->get('translate');
+
+        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.54')) {
+            $message = new \Omeka\Stdlib\Message(
+                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+                'Common', '3.4.54'
+            );
+            throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+        }
+    }
+
     public function attachListeners(SharedEventManagerInterface $sharedEventManager): void
     {
         $sharedEventManager->attach(

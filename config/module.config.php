ab6213972d82dad454cb42c97c5f129b281a8473
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-07-07 00:00:00 +0000

    Fixed translations.

--- config/module.config.php
+++ config/module.config.php
@@ -68,6 +68,16 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'singlesignon' => [
         'config' => [
             'singlesignon_services' => [

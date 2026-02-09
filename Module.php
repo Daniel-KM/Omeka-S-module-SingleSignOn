358cc75b33f14767241738af5ebaf9634c4662e8
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-02-09 00:00:00 +0000

    Released version 3.4.26.

--- Module.php
+++ Module.php
@@ -21,7 +21,7 @@ use OneLogin\Saml2\Utils;
 /**
  * Single Sign-On.
  *
- * @copyright Daniel Berthereau, 2023-2025
+ * @copyright Daniel Berthereau, 2023-2026
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  */
 class Module extends AbstractModule

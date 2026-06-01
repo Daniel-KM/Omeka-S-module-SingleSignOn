a0f5666a285e66e4c5c18f3e055d22e471cba674
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-06-01 00:00:00 +0000

    Released version 3.4.28.

--- Module.php
+++ Module.php
@@ -54,12 +54,11 @@ class Module extends AbstractModule
     protected function preInstall(): void
     {
         $services = $this->getServiceLocator();
-        $plugins = $services->get('ControllerPluginManager');
-        $translate = $plugins->get('translate');
+        $translator = $services->get('MvcTranslator');
 
         if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.86')) {
             $message = new \Omeka\Stdlib\Message(
-                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+                $translator->translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
                 'Common', '3.4.86'
             );
             throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
